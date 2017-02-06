2c45637d0935e0a4acce45194bf833033df1905b
slashb <slashb410@gmail.com>
2017-02-06 16:12:15 +0900

    ...

--- app/src/main/java/com/google/slashb410/exgroup/MainActivity.java
+++ app/src/main/java/com/google/slashb410/exgroup/MainActivity.java
@@ -11,9 +11,9 @@ import com.google.slashb410.exgroup.ui.IntroActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    //추가
+    // 추가
     // 추가2
-    // 추가3 test
+    // 추가3 test ..
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
