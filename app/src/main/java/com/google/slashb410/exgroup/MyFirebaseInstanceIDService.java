a78e038813c61fb2f0751fb6822a777e9508e5a0
slashb <slashb410@gmail.com>
2017-02-09 15:57:29 +0900

    sodami fcm + toggle event

---
+++ app/src/main/java/com/google/slashb410/exgroup/MyFirebaseInstanceIDService.java
@@ -0,0 +1,9 @@
+package com.google.slashb410.exgroup;
+
+import android.app.Service;
+
+/**
+ * Created by Tacademy on 2017-02-09.
+ */
+public class MyFirebaseInstanceIDService extends Service {
+}
