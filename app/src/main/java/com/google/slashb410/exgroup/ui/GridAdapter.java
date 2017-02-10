aefe812dede3ee744da3a9b0aa404932eb439b3a
slashb <slashb410@gmail.com>
2017-02-10 09:36:19 +0900

    sodami GridAdapter 변경내역

--- app/src/main/java/com/google/slashb410/exgroup/ui/GridAdapter.java
+++ app/src/main/java/com/google/slashb410/exgroup/ui/GridAdapter.java
@@ -2,12 +2,22 @@ package com.google.slashb410.exgroup.ui;
 
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.slashb410.exgroup.R;
+import com.google.slashb410.exgroup.db.E;
+import com.google.slashb410.exgroup.ui.group.create.GroupAddActivity;
+import com.google.slashb410.exgroup.ui.group.room.GroupHomeActivity;
+import com.google.slashb410.exgroup.util.U;
+
+import static com.google.slashb410.exgroup.db.E.KEY.GROUP_MAX;
 
 /**
  * Created by Tacademy on 2017-02-01.
@@ -50,79 +60,76 @@ class GridAdapter extends BaseAdapter {
 
         TextView textView;
         CardView cardView;
-//<<<<<<< HEAD
 
-//        if (position == groupName.length) {
-//            cardView = (CardView) convertView.findViewById(R.id.group_add_cardview);
-//            cardView.setVisibility(View.VISIBLE);
-//            cardView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    U.getInstance().goNext(v.getContext(), GroupAddActivity.class, false);
-//                }
-//            });
-//        } else if (position == R.dimen.GROUP_MAX) {
-//            cardView = (CardView) convertView.findViewById(R.id.group_add_cardview);
-//            cardView.setVisibility(View.VISIBLE);
-//            cardView.setClickable(false);
-//            ImageView imageView = (ImageView) convertView.findViewById(R.id.group_add_img);
-//            imageView.setVisibility(View.GONE);
-//            textView = (TextView) convertView.findViewById(R.id.group_addTxt);
-//            textView.setText("그룹은 최대 5개까지 소속될 수 있습니다.");
-//            textView.setTextSize(R.dimen.text_small);
-//
-//        } else
-//
-//        {
-//            cardView = (CardView) convertView.findViewById(R.id.group_cardview);
-//            textView = (TextView) convertView.findViewById(R.id.group_name_card);
-//            textView.setText(groupName[position]);
-//
-//
-//            cardView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    U.getInstance().goNext(v.getContext(), GroupHomeActivity.class, false);
-//                }
-//            });
-////        }
-//        if (position == E.KEY.GROUP_MAX) {
-//            cardView = (CardView) convertView.findViewById(R.id.group_add_cardview);
-//            cardView.setVisibility(View.VISIBLE);
-//            cardView.setClickable(false);
-//            cardView.setBackgroundResource(R.color.babyPink);
-//            ImageView imageView = (ImageView) convertView.findViewById(R.id.group_add_img);
-//            imageView.setVisibility(View.GONE);
-//            textView = (TextView) convertView.findViewById(R.id.group_addTxt);
-//            textView.setText("그룹은 최대 5개까지 소속될 수 있습니다.");
-//            textView.setTextColor(Color.WHITE);
-//
-//
-//        } else if (position == groupName.length) {
-//            cardView = (CardView) convertView.findViewById(R.id.group_add_cardview);
-//            cardView.setVisibility(View.VISIBLE);
-//            cardView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    U.getInstance().goNext(v.getContext(), GroupAddActivity.class, false);
-//                }
-//            });
-//        } else {
-//            cardView = (CardView) convertView.findViewById(R.id.group_cardview);
-//            textView = (TextView) convertView.findViewById(R.id.group_name_card);
-//            textView.setText(groupName[position]);
-//
-//
-//            cardView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    U.getInstance().goNext(v.getContext(), GroupHomeActivity.class, false);
-//                }
-//            });
-//        }
+        if (position == groupName.length) {
+            cardView = (CardView) convertView.findViewById(R.id.group_add_cardview);
+            cardView.setVisibility(View.VISIBLE);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    U.getInstance().goNext(v.getContext(), GroupAddActivity.class, false);
+                }
+            });
+        } else if (position == E.KEY.GROUP_MAX) {
+            cardView = (CardView) convertView.findViewById(R.id.group_add_cardview);
+            cardView.setVisibility(View.VISIBLE);
+            cardView.setClickable(false);
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.group_add_img);
+            imageView.setVisibility(View.GONE);
+            textView = (TextView) convertView.findViewById(R.id.group_addTxt);
+            textView.setText("그룹은 최대 5개까지 소속될 수 있습니다.");
+            textView.setTextSize(R.dimen.text_small);
+
+        } else
+
+        {
+            cardView = (CardView) convertView.findViewById(R.id.group_cardview);
+            textView = (TextView) convertView.findViewById(R.id.group_name_card);
+            textView.setText(groupName[position]);
 
 
-        return convertView;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    U.getInstance().goNext(v.getContext(), GroupHomeActivity.class, false);
+                }
+            });
+        }
+            if (position == GROUP_MAX) {
+                cardView = (CardView) convertView.findViewById(R.id.group_add_cardview);
+                cardView.setVisibility(View.VISIBLE);
+                cardView.setClickable(false);
+                cardView.setBackgroundResource(R.color.babyPink);
+                ImageView imageView = (ImageView) convertView.findViewById(R.id.group_add_img);
+                imageView.setVisibility(View.GONE);
+                textView = (TextView) convertView.findViewById(R.id.group_addTxt);
+                textView.setText("그룹은 최대 5개까지 소속될 수 있습니다.");
+                textView.setTextColor(Color.WHITE);
 
+
+            } else if (position == groupName.length) {
+                cardView = (CardView) convertView.findViewById(R.id.group_add_cardview);
+                cardView.setVisibility(View.VISIBLE);
+                cardView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        U.getInstance().goNext(v.getContext(), GroupAddActivity.class, false);
+                    }
+                });
+            } else {
+                cardView = (CardView) convertView.findViewById(R.id.group_cardview);
+                textView = (TextView) convertView.findViewById(R.id.group_name_card);
+                textView.setText(groupName[position]);
+
+
+                cardView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        U.getInstance().goNext(v.getContext(), GroupHomeActivity.class, false);
+                    }
+                });
+            }
+            return convertView;
+        }
     }
-}
+
